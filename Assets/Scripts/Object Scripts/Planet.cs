1c77990f22292131f62f7239c88f1e3f91ec88e0
DavidBocek <dmbocek@gmail.com>
2014-01-30 17:51:23 -0500

    changed some colors on enemys and added trails and such

--- Assets/Scripts/Object Scripts/Planet.cs
+++ Assets/Scripts/Object Scripts/Planet.cs
@@ -4,41 +4,43 @@ using System.Collections;
 public class Planet : MonoBehaviour {
 
 	public float normalPulseRadius;
-	public float tempPulseDelay;
-	public float pulseTime;
 	public Transform pulseObjectTrans;
-
-	private float tempPulseTimer;
+	public int type;
+	public bool playing {get; set;}
+	public float pulseTime;
+	public AudioSource musicSource;
+	
 	private AudioSource pulseSound;
 	private float initialScale;
+	private int freq;
+	private int samplesPerBeat;
 
 	private float dt;
 
 	// Use this for initialization
 	void Start () {
 		pulseSound = GetComponent<AudioSource>();
-		tempPulseTimer = tempPulseDelay;
 		pulseObjectTrans.gameObject.GetComponentInChildren<MeshRenderer>().enabled = false;
 		initialScale = pulseObjectTrans.localScale.x;
+		freq = audio.clip.frequency;
+		samplesPerBeat = freq/3; //180 bpm => 3 beats per second
+		playing = true;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	private bool started = false;
+	void Update(){
 		dt = Time.smoothDeltaTime;
-		if (tempPulseTimer<= 0){
-			pulseObjectTrans.gameObject.GetComponentInChildren<MeshRenderer>().enabled = true;
-			StartCoroutine("Pulse",normalPulseRadius);
-			pulseSound.Play();
-			tempPulseTimer = tempPulseDelay;
-		} else {
-			tempPulseTimer -= dt;
+		if (!started){
+			StartCoroutine("SyncToAudio");
+			started = true;
 		}
 	}
 
 
-	IEnumerator Pulse(float radius){
+	IEnumerator PulseVisual(float radius){
 		float newScale;
 		Color c = pulseObjectTrans.gameObject.GetComponentInChildren<MeshRenderer>().material.color;
+		pulseObjectTrans.gameObject.GetComponentInChildren<MeshRenderer>().enabled = true;
 		for (float t = 0; t<=1f; t += dt/pulseTime){
 			//for right now, lerp between 0 and 1 alpha
 			c.a = t;
@@ -62,5 +64,68 @@ public class Planet : MonoBehaviour {
 		pulseObjectTrans.gameObject.GetComponentInChildren<MeshRenderer>().enabled = false;
 	}
 
+	private bool completedInitialDelay = false;
+	private int currentSamples = 0;
+	private int deltaSamples;
+	private int lastSample = 0;
+	IEnumerator SyncToAudio(){
+		while (playing){
+			switch (type){
+			case 0:
+				//play at the first beat of measures 1, 3, 5, 7, etc.
+				//aka every 8 beats
+				while (currentSamples < 8 * samplesPerBeat){
+					deltaSamples = musicSource.timeSamples - lastSample;
+					if (deltaSamples < 0){
+						deltaSamples = musicSource.timeSamples;
+						lastSample = 0;
+					}
+					currentSamples += deltaSamples;
+					lastSample = musicSource.timeSamples;
+					yield return null;
+				}
+				currentSamples = 0;
+				pulseSound.Play ();
+				StartCoroutine("PulseVisual",normalPulseRadius);
+				break;
+			case 1:
+				//play at the 3rd beat of every measure
+				//aka every 4 beats starting on the 3rd
+				if (!completedInitialDelay){
+					while (currentSamples < 3 * samplesPerBeat){
+						deltaSamples = musicSource.timeSamples - lastSample;
+						if (deltaSamples < 0){
+							deltaSamples = musicSource.timeSamples;
+							lastSample = 0;
+						}
+						currentSamples += deltaSamples;
+						lastSample = musicSource.timeSamples;
+						yield return null;
+					}
+					pulseSound.Play ();
+					StartCoroutine("PulseVisual",normalPulseRadius);
+					completedInitialDelay = true;
+				}
+				while (currentSamples < 4 * samplesPerBeat){
+					deltaSamples = musicSource.timeSamples - lastSample;
+					if (deltaSamples < 0){
+						deltaSamples = musicSource.timeSamples;
+						lastSample = 0;
+					}
+					currentSamples += deltaSamples;
+					lastSample = musicSource.timeSamples;
+					yield return null;
+				}
+				currentSamples = 0;
+				pulseSound.Play();
+				StartCoroutine("PulseVisual",normalPulseRadius);
+				break;
+			case 2:
+				//play at the 2nd beat of measures 1, 3, 5, 7, etc. and the 4th beat of measures 2, 4, 6, 8, etc.
 
+				break;
+			}
+			yield return null;
+		}
+	}
 }
